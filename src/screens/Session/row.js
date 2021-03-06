692807e94751e376a8631d96eb9490ec01208719
BenjaminPcrd <benjamin.picard0@gmail.com>
2021-03-06 15:56:41 +0100

    svg icons

--- src/screens/Session/row.js
+++ src/screens/Session/row.js
@@ -6,27 +6,40 @@ import {
     StyleSheet
 } from 'react-native'
 
-import Icon from 'react-native-vector-icons/Ionicons'
+import SportClimbingIcon from "../../assets/route_type_sport.svg"
+import TradClimbingIcon from "../../assets/route_type_trad.svg"
+import IceClimbingIcon from "../../assets/route_type_ice.svg"
+import AidClimbingIcon from "../../assets/route_type_aid.svg"
+import BoulderingIcon from "../../assets/route_type_bouldering.svg"
 
 import { translate } from '../../translations'
 
 const Row = ({ item }) => {
     return (
         <View style={styles.row}>
-            <View style={{flexDirection: 'row'}}>
-                <View style={{flex: 2}}>
-                    <Text style={styles.name}>{item.name}</Text>
-                </View>
-                <View style={{flex: 1}}>
-                    <Text style={styles.grade}>{item.grade.grade}</Text>
-                </View>
+            <View style={{flex: 1}}>
+                {item.type === 'SPORT_CLIMBING' && <SportClimbingIcon width={50} height={50} fill={"black"}/>}
+                {item.type === 'TRAD_CLIMBING' && <TradClimbingIcon width={50} height={50} fill={"black"}/>}
+                {item.type === 'ICE_CLIMBING' && <IceClimbingIcon width={50} height={50} fill={"black"}/>}
+                {item.type === 'AID_CLIMBING' && <AidClimbingIcon width={50} height={50} fill={"black"}/>}
+                {item.type === 'BOULDERING' && <BoulderingIcon width={50} height={50} fill={"black"}/>}
             </View>
-            <View style={{flexDirection: 'row'}}>
-                <View style={{flex: 2}}>
-                    <Text style={styles.mode}>{translate(item.mode)}</Text>
+            <View style={{flex: 5}}>
+                <View style={{flexDirection: 'row'}}>
+                    <View style={{flex: 2}}>
+                        <Text style={styles.name}>{item.name}</Text>
+                    </View>
+                    <View style={{flex: 1}}>
+                        <Text style={styles.grade}>{item.grade.grade}</Text>
+                    </View>
                 </View>
-                <View style={{flex: 1}}>
-                    <Text style={styles.style}>{translate(item.style)}</Text>
+                <View style={{flexDirection: 'row'}}>
+                    <View style={{flex: 2}}>
+                        <Text style={styles.mode}>{translate(item.mode)}</Text>
+                    </View>
+                    <View style={{flex: 1}}>
+                        <Text style={styles.style}>{translate(item.style)}</Text>
+                    </View>
                 </View>
             </View>
         </View>
@@ -35,8 +48,8 @@ const Row = ({ item }) => {
 
 const styles = StyleSheet.create({
     row: {
-        paddingVertical: 10,
-        paddingHorizontal: 20
+        padding: 10,
+        flexDirection: 'row'
     },
     name: {
         color: 'darkblue',
