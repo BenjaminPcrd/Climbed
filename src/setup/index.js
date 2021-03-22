fe387f68e3f72a5121f8905d7b51493a4aefe9ea
BenjaminPcrd <benjamin.picard0@gmail.com>
2021-03-22 08:50:47 +0100

    load saved sessions

--- src/setup/index.js
+++ src/setup/index.js
@@ -1,6 +1,22 @@
-import React from 'react'
-
-import 'react-native-gesture-handler'
+import React, { useEffect} from 'react'
 
 import App from '../App'
-export default () => <App/>
+
+import 'react-native-gesture-handler'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+const Setup = () => {
+
+    const setSessions = async() => {
+        //await AsyncStorage.setItem('@sessions', JSON.stringify(require('../../sessions.json')))
+    }
+
+    useEffect(() => {
+        setSessions()
+    }, [])
+
+    return <App/>
+}
+
+export default Setup
+
